39207928fe3a429bbdb06f87fde15a6e32744a41
Naveen <naveensalgaonker@gmail.com>
2021-01-27 18:32:29 +0530

    improvised hero section

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
-const navBtn = document.querySelector(".nav-btn");
-const navLinks = document.querySelectorAll(".nav-links a");
+const navBtn = document.querySelector('.nav-btn');
+const navLinks = document.querySelectorAll('.nav-links a');
+const skillsContain = document.querySelector('.skill-1');
 
-navBtn.addEventListener("click", () => {
+navBtn.addEventListener('click', () => {
 	navLinks.forEach((item) => {
-		item.classList.toggle("show");
+		item.classList.toggle('show');
 	});
-	document.querySelector(".line-1").classList.toggle("rotate-1");
-	document.querySelector(".line-2").classList.toggle("rotate-2");
-	document.querySelector(".line-3").classList.toggle("rotate-3");
+	document.querySelector('.line-1').classList.toggle('rotate-1');
+	document.querySelector('.line-2').classList.toggle('rotate-2');
+	document.querySelector('.line-3').classList.toggle('rotate-3');
 });
+
+observer = new IntersectionObserver((entries) => {
+	console.log(entries);
+	if (entries.intersectionRatio > 0) {
+		console.log('intersected');
+	}
+});
+
+observer.observe(skillsContain);
